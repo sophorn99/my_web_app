9a1efce70074ca6d10cf5e1bfd8e57cd8d77e599
sophorn.morn <sophorn.morn@PNC.LAN>
2019-09-01 17:24:30 +0700

    button

---
+++ js/index.js
@@ -0,0 +1,26 @@
+
+function resizeButtonPeople() {
+  let width = 0;
+  let buttons = document.getElementsByClassName('button-people');
+
+  for (let button of buttons) {
+    button.style.width = "auto";
+  }
+
+  for (let button of buttons) {
+    if (button.offsetWidth > width) { 
+      width = button.offsetWidth;
+    }
+  }
+  for (let button of buttons) {
+    button.style.width = width + 1 +"px"
+    button.style.width = `${width + 1}px`;
+  } 
+}
+
+
+window.onload = function() {
+  resizeButtonPeople();
+}
+
+
